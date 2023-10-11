d9980c990d394743c7464e83c87bdaa53ce36e5d
Oishy Barua <oishy.cmt9@gmail.com>
2023-10-11 06:39:34 +0600

    init

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function create(){
+
+    }
+
+    public function show(){
+
+    }
+
+    public function updateInfo(){
+
+    }
+
+    public function deleteList(){
+
+    }
+}
